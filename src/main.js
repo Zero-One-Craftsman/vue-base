7130760a89219beddc35ece478035037cf2cb6c3
TheEverlyn <oncwnuPHraQE2iCPc4byZrcyKQ8s@git.weixin.qq.com>
2024-12-29 15:06:01 +0800

    base edit

---
+++ src/main.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: TheEverlyn oncwnuPHraQE2iCPc4byZrcyKQ8s@git.weixin.qq.com
+ * @Date: 2024-12-08 15:10:53
+ * @LastEditors: TheEverlyn oncwnuPHraQE2iCPc4byZrcyKQ8s@git.weixin.qq.com
+ * @LastEditTime: 2024-12-29 15:03:57
+ * @FilePath: \vue-base\src\main.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import { createApp } from 'vue';
+import App from './App.vue';
+ 
+createApp(App).mount('#app');
