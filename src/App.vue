fecb3928b433139aae6c10870c00455760e39fe9
TheEverlyn <oncwnuPHraQE2iCPc4byZrcyKQ8s@git.weixin.qq.com>
2024-12-29 22:11:50 +0800

    edit

--- src/App.vue
+++ src/App.vue
@@ -2,28 +2,31 @@
  * @Author: TheEverlyn oncwnuPHraQE2iCPc4byZrcyKQ8s@git.weixin.qq.com
  * @Date: 2024-12-29 15:39:59
  * @LastEditors: TheEverlyn oncwnuPHraQE2iCPc4byZrcyKQ8s@git.weixin.qq.com
- * @LastEditTime: 2024-12-29 16:19:07
+ * @LastEditTime: 2024-12-29 22:09:53
  * @FilePath: \vue-init\src\App.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 <template>
   <div>
     <img :src="imageUrl"></img>
-    <div>{{ message }}</div>
+    // #INSERT TEXT HERE
   </div>
 </template>
  
 <script>
+// #INSERT IMPORT HERE
 export default {
   data() {
     return {
       imageUrl:'./assets/xxx.png',
-      message: '你好欢迎来到集成项目中心!'
     }
   },
   setup(){
-    
-  }
+    // #INSERT METHOD HERE
+    return {
+      // #INSERT RETURN HERE
+    }
+  },
 }
 </script>
  
